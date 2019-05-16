b7cd6b962a8eca3b0255790aa018c389adf92665
maityado <50463497+maityado@users.noreply.github.com>
2019-05-16 15:03:26 -0500

    primer repositorio de apirest

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+app.get("/users", (req, res) => {
+  let users = [
+    { name: "Jhon", lastName: "Doe", age: 30 },
+    { name: "María", lastName: "Sharápova", age: 32 }
+  ];
+  res.send(users);
+});
+app.get("/alumnos", (req, res) => {
+  let alumnos = [
+    { ID: 0, nombre: "Juan", apellido: "Martinez"  },
+    { ID: 1, nombre: "María", apellido: "Vargas" },
+    { ID: 2, nombre: "Pedro", apellido: "Gomez" },
+    { ID: 3, nombre: "Jesus", apellido: "Argumedo" },
+    { ID: 4, nombre: "Daniela", apellido: "Petro" },
+  ];
+  res.send(alumnos);
+});
+app.listen(3000, () => {
+  console.log("Server on port http://localhost:3000");
+});
